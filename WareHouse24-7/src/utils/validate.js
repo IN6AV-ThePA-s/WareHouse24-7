6755b13e34821735f940b45cd9e2df5680b96850
jcalderon-2018148 <jcalderon-2018148@kinal.edu.gt>
2023-04-27 00:59:04 -0600

    Se creó CRUD de User

--- WareHouse24-7/src/utils/validate.js
+++ WareHouse24-7/src/utils/validate.js
@@ -19,3 +19,17 @@ exports.check = async(password, hash) => {
         return err;
     }
 }
+
+exports.validateData = (data) => {
+    let keys = Object.keys(data), msg = ''
+
+    for(let key of keys) {
+        if(data[key] !== null &&
+            data[key] !== undefined &&
+            data[key] !== '') continue
+
+        msg += `Param ${key} is required \n`
+    }
+
+    return msg.trim()
+}
