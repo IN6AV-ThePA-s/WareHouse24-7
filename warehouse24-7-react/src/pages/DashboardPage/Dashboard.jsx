0a1599d8a5a073ec21e8a889ecf051173164baca
gmatta-2021223 <gmatta-2021223@kinal.edu.gt>
2023-04-28 17:23:36 -0600

    Se arregló sidebar

--- warehouse24-7-react/src/pages/DashboardPage/Dashboard.jsx
+++ warehouse24-7-react/src/pages/DashboardPage/Dashboard.jsx
@@ -2,33 +2,35 @@ import React, { useState } from 'react'
 import '../../css/bootstrap.min.css'
 import '../DashboardPage/styleDashboard.css'
 import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom'
 
 export const Dashboard = () => {
     const [open, setOpen]=useState(false)
     return (
         <>
 
-            <header className="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow sticky-top">
+            <header className="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow sticky-top justify-content align-items-center">
                 {/* <img className='ms-3' src={logo} width='35px' height='35px' /> */}
-                <h1 className="brush navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6">WH 24/7</h1>
+                <div className='navbar-nav navbar-brand col-md-3 col-lg-2 me-0 px-3' style={{backgroundColor: '#ffffff'}}>
+                    <Link className="brush">WH 24/7</Link>
+                </div>
                 <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" onClick={()=>setOpen(!open)}
                      aria-controls="sidebarMenu" aria-expanded="false"
                     aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <input className="form-control form-control-dark w-100 rounded-0 border-0" type="text" placeholder="Search"
-                    aria-label="Search" />
-                <div className="navbar-nav">
+                {/* <div className="navbar-nav">
                     <div className="nav-item text-nowrap">
                         <a className="nav-link px-3" href="#">Sign out</a>
                     </div>
-                </div>
+                </div> */}
+                
             </header>
 
             <div className="container-fluid">
                 <div className="row">
 
-                    <nav id="sidebarMenu" className={`col-md-3 col-lg-2 d-md-block bg-light sidebar ${open ? 'collapsing' :'collapse'}`} style={open?{height: '740px'}:{'':''}}>
+                    <nav id="sidebarMenu" className={`col-md-3 col-lg-2 d-md-block bg-light sidebar ${open ? 'collapsing' :'collapse'}`} style={open?{height: 'auto'}:{'':''}}>
                         <div className="position-sticky pt-3 sidebar-sticky">
                             <h5
                                 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-uppercase">
@@ -37,7 +39,7 @@ export const Dashboard = () => {
                                     <span data-feather="plus-circle" className="align-text-bottom"></span>
                                 </a>
                             </h5>
-                            <ul className="nav flex-column">
+                            <ul className="nav flex-column d-flex">
                                 <li className="nav-item">
                                     <a className="nav-link active bi bi-house-door" aria-current="page" href="#">
                                         <span className="align-text-bottom bi"> </span>
@@ -77,32 +79,42 @@ export const Dashboard = () => {
                                     <span data-feather="plus-circle" className="align-text-bottom"></span>
                                 </a>
                             </h6>
-                            <ul className="nav flex-column mb-2">
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">
-                                        <span data-feather="file-text" className="align-text-bottom"></span>
-                                        Current month
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">
-                                        <span data-feather="file-text" className="align-text-bottom"></span>
-                                        Last quarter
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">
-                                        <span data-feather="file-text" className="align-text-bottom"></span>
-                                        Social engagement
-                                    </a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">
-                                        <span data-feather="file-text" className="align-text-bottom"></span>
-                                        Year-end sale
-                                    </a>
-                                </li>
-                            </ul>
+                            <ul className="nav h-50 d-flex flex-column justify-content-between mb-2">
+                                <div>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">
+                                            <span data-feather="file-text" className="align-text-bottom"></span>
+                                            Current month
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">
+                                            <span data-feather="file-text" className="align-text-bottom"></span>
+                                            Last quarter
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">
+                                            <span data-feather="file-text" className="align-text-bottom"></span>
+                                            Social engagement
+                                        </a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">
+                                            <span data-feather="file-text" className="align-text-bottom"></span>
+                                            Year-end sale
+                                        </a>
+                                    </li>    
+                                </div>  
+                                <div className='fixed-bottom'>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="#">
+                                            <span data-feather="file-text" className="align-text-bottom"></span>
+                                            Log Out
+                                        </a>
+                                    </li> 
+                                </div>
+                            </ul> 
                         </div>
                     </nav>
 
